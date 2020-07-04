720ed4b83cbfb630f29312fa5fa0bbcc049a366d
Kabali Michael <fitzerkisakyamukama03@gmail.com>
2020-07-04 04:18:29 +0300

    registering service with angular injector

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './products';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class  productServices {
   getProducts(): IProduct[] {
     return [
